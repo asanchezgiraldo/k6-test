3625e33aba26cce369a8de708ac42d904836dcc4
alejandro <alejandro.sanchez@qantasloyalty.com>
2019-04-02 22:24:10 +1100

    add password check and other checks

--- test/password-test.js
+++ test/password-test.js
@@ -1,5 +1,8 @@
 import http from "k6/http";
 import { check, sleep } from "k6";
+import { Rate } from "k6/metrics";
+
+var myFailRate = new Rate("failed requests");
 
 
 export let options = {
@@ -10,6 +13,9 @@ export let options = {
         { duration: "30s", target: 200 },
         { duration: "30s", target: 0 }
     ],
+    thresholds: {
+        "failed requests": ["rate<0.1"],
+    }
 };
 
 
@@ -17,8 +23,10 @@ export default function() {
     let txt = Math.random().toString(36).substring(7);
 
     let res = http.get("https://guarded-scrubland-78590.herokuapp.com/api/hash?password="+txt);
+    myFailRate.add(res.json().password == "");
     check(res, {
             "status was 200": (r) => r.status == 200,
+            "transaction time OK": (r) => r.timings.duration < 1500,
             "passowrd is not empty": (r) => r.json().password != ""
         });
     sleep(1);
